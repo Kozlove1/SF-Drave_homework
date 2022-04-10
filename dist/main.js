98086d4b3a8f698f82b162a5609dddd91fd7e682
Kozlove1 <kozlove1661@gmail.com>
2022-04-10 16:14:10 +0300

    Наладил пакеты зависимостей Webpack поставил и подключил css к Webpack

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";const t=document.querySelectorAll(".questions__box-collapsible");for(let e=0;e<t.length;e++)t[e].addEventListener("click",(function(){this.classList.toggle("active");const t=this.nextElementSibling;t.style.maxHeight?t.style.maxHeight=null:t.style.maxHeight=t.scrollHeight+"em"}))})();
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js'
+    },
+    plugins: [new MiniCssExtractPlugin()],
+      module: {
+        rules: [
+          { test: /\.css$/,
+           use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                esModule: true,
+              },
+            },
+            'css-loader',
+       ], }
+        ]
+      }
+}

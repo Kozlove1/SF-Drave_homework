899ae01a61d0f2fd6b7001acaa8eb5406c446e0d
Kozlove1 <kozlove1661@gmail.com>
2022-04-24 16:13:14 +0300

    Установка Pug

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,32 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+
 
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/FaqNew.js',
     output: {
         filename: 'main.js'
     },
     mode: 'development',
-    plugins: [new MiniCssExtractPlugin()],
+    plugins: [
+      new MiniCssExtractPlugin(),
+      new HtmlWebpackPlugin({
+        template: "C:/Users/User/VScode/SF-Drave_homework/src/FAQ.pug"
+    }),
+      new OptimizeCssAssetsWebpackPlugin(),
+      new TerserWebpackPlugin(),
+    ],
+
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new OptimizeCssAssetsWebpackPlugin(),
+        new TerserWebpackPlugin(),
+        ],
+    },
       module: {
         rules: [
           { test: /\.css$/,
@@ -19,7 +38,14 @@ module.exports = {
               },
             },
             'css-loader',
-       ], }
+       ], },
+       {
+       test: /\.pug$/,
+       loader: 'pug-loader',
+       options: {
+       pretty: true
+       }
+      },
         ]
       }
 }

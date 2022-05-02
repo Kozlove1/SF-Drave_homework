8358dab4e04352352434a6cabc6cf4abd958d931
Kozlove1 <kozlove1661@gmail.com>
2022-05-02 22:35:42 +0300

    новые зависимости для eslint и исключения для проверки

--- src/FAQ/FaqNew.js
+++ src/FAQ/FaqNew.js
@@ -1,14 +1,17 @@
-/* eslint-disable @lwc/lwc/no-document-query */
-const coll = document.querySelectorAll(".questions__box-collapsible");
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
 
-for (let i = 0; i < coll.length; i++){
-    coll[i].addEventListener("click", function (){
-        this.classList.toggle("active");
-        const content = this.nextElementSibling;
-        if (content.style.maxHeight){
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "em";
-        } 
-    });
+
+const coll = document.querySelectorAll('.questions__box-collapsible');
+
+for (let i = 0; i < coll.length; i++) {
+  coll[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+    const content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = `${content.scrollHeight}em`;
+    }
+  });
 }
